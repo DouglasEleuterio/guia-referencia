71f47245e614a989a2438a4f5a2a379d5596f4df
Douglas Eleuterio <douglas-negocios@hotmail.com>
2020-01-16 16:54:42 -0300

    first commit

---
+++ src/main/java/com/douglas/guiareferencia/GuiaReferenciaApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.guiareferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaReferenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuiaReferenciaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/douglas/guiareferencia/GuiaReferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.guiareferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaReferenciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
